cbe98441eb77ec40881732d5f51e019ad5839218
jroiceee <jroicealdeza@gmail.com>
2022-07-16 01:46:27 +0800

    add browser detection for safari

--- src/UIComponents/initial.js
+++ src/UIComponents/initial.js
@@ -4,12 +4,53 @@ import background from '../assets/background.jpeg';
 export default function loadInitialFiles() {
     document.querySelector('title').textContent = "WeatherApp";
     document.querySelector('head').appendChild(createIconLink());
-    document.body.style.backgroundImage = `url(${background})`;
+    setBackground();
 }
+
 function createIconLink() {
     const linkTag = document.createElement('link');
     linkTag.setAttribute('rel', "shortcut icon");
     linkTag.setAttribute('href', icon);
     linkTag.setAttribute('type', 'image/x-icon');
     return linkTag;
+}
+
+//background: fixed is not supported in safari
+function setBackground() {
+    if(isSafari()) {
+        return;
+    }
+    document.body.style.backgroundImage = `url(${background})`;
+}
+
+
+function isSafari() {
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    {
+        return false;
+    }
+    else if(navigator.userAgent.indexOf("Edg") != -1 )
+    {
+        return false;
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+        return false;
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+        return true;
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+        return false;
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+        return false;
+    }  
+    else 
+    {
+       return false;
+    }
 }
